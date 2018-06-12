0f31b008eaa0c5f54aad1059942c4b2a41bd6c4a
Furyou <kenji111.fujimoto@gmail.com>
2018-06-12 11:50:02 +0200

    ownable

--- ownable.sol
+++ ownable.sol
@@ -7,8 +7,6 @@ pragma solidity ^0.4.0;
 contract Ownable {
   address public owner;
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
